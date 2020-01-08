0078e5eac31625de53b249380c5f5b53294ba421
Gemma Hentsch <gemma@halfapenguin.com>
2020-01-08 14:18:26 +0000

    Tasks update

--- server/src/tasks.py
+++ server/src/tasks.py
@@ -4,7 +4,7 @@ from server.src.auditing import AuditLog
 from server.src.celery import celery
 # from server.src.datamanagement.git import push_to_origin, commit_file
 from server.src.messaging import NotificationService
-
+import boto3
 # @celery.task(queue="communication")
 # def push_to_origin_task():
 #     push_to_origin()
@@ -37,6 +37,37 @@ def send_document_done_notification(collection, document, user):
         'task_id': task.id,
     }
     return json_dic
+
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, long_poll_sns.s(), expires=10)
+
+
+@celery.task(queue="periodic")
+def long_poll_sns():
+    # Create SQS client
+    sqs = boto3.client('sqs')
+
+    queue_url = 'https://sqs.eu-west-2.amazonaws.com/519411350235/BratAnnotationsQueue'
+
+    # Long poll for message on provided SQS queue
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        WaitTimeSeconds=20
+    )
+    print(f"Long Poll Complete - {response}")
+
+
 
 # @celery.task(queue="internal")
 # def _git_commit_task(user, relative_path):
